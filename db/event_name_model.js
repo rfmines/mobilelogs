c44f45f2e0f57af86d02fa084a201aa41d020663
rfmines <rfmines@tsunali.com>
2016-11-08 14:47:14 -0800

    fixes

--- db/event_name_model.js
+++ db/event_name_model.js
@@ -8,20 +8,7 @@ var db = mongoose.connection;
 var env = process.env.NODE_ENV || 'production';
 var config = require('./config')[env];
 var db_config = config.database;
-/*
-db.on('error', function (err) {
-  logger.fatal('log_model error connecting to mongodb: ', err)
-});
 
-db.on('connected', function () {
-  logger.info('Database connected ');
-})
-db.on('disconnected', function () {
-  logger.info('Database disconnected');
-})
-
-//mongoose.connect('mongodb://' + db_config.host + '/'+ db_config.db, {user: db_config.user, pass: db_config.password});
-*/
 var Schema = mongoose.Schema;
 var ObjectId = Schema.ObjectId;
 
--- db/event_type_model.js
+++ db/event_type_model.js
@@ -1,29 +1,13 @@
 var mongoose = require('mongoose');
 var log4js = require('log4js');
 var logger = log4js.getLogger('applog');
-
+var Promise = require('promise');
 var db = mongoose.connection;
-
 var env = process.env.NODE_ENV || 'production';
 var config = require('./config')[env];
 var db_config = config.database;
-/*
-db.on('error', function (err) {
-  logger.fatal('log_model error connecting to mongodb: ', err)
-});
-
-db.on('connected', function () {
-  logger.info('Database connected ');
-})
-db.on('disconnected', function () {
-  logger.info('Database disconnected');
-})
-
-//mongoose.connect('mongodb://' + db_config.host + '/'+ db_config.db, {user: db_config.user, pass: db_config.password});
-*/
 var Schema = mongoose.Schema;
 var ObjectId = Schema.ObjectId;
-
 
 var eventTypeDictionarySchema = new Schema({
   id: String,
@@ -33,42 +17,69 @@ var eventTypeDictionarySchema = new Schema({
 var EventTypes = mongoose.model('EventTypes', eventTypeDictionarySchema);
 
 module.exports = {
-  getEventType: function (id) {
-    EventTypes.findOne({'id': id}, 'value', function (err, event) {
-      if (err) throw (err);
-      return (event.value)
-    })
-  },
-  addEventType: function (event) {
-    EventTypes.save(event, function (err) {
-      if (err) throw (err);
-      return (true)
-    })
-  },
-  removeEventType: function (id) {
-    EventTypes.findOneAndRemove({field: 'newValue'}, function (err) {
-      if (err) throw (err);
-      return (true)
-    })
-  },
+  get: function(id) {
+    return new Promise(function (resolve, reject) {
+      EvetnTypes.findById(id, function (err, item) {
+        if (err) reject( err );
 
-  listEventTypes: function () {
-    EventTypes.find({}, function (err, events) {
-      if (err) throw(e)
-      var eventsMap = {};
-      console.log('found ' + events.length + " events")
-      events.forEach(function(event) {
-        console.log(event.name + ' ' + event.id)
-        eventsMap[event._id] = event;
+        resolve( item );
+      })
+    });
+  },
+  remove: function (id) {
+    return new Promise(function (resolve, reject) {
+      EvetnTypes.remove({_id: id}, function (err) {
+        if (err) reject( err );
+
+        resolve( 'ok' );
       });
-      return eventsMap;
-    })
+    });
   },
 
-  initEventType: function (id) {
-    EventTypes.findOne({'id': id}, 'value', function (err, event) {
-      if (err) throw (err);
-      return (event.value)
-    })
+  list: function (query) {
+    if (query == null) { query = {}; }
+
+    return new Promise(function (resolve, reject) {
+      EvetnTypes.find(query, function (err, items) {
+        if (err) reject( err );
+
+        resolve( items );
+      });
+    });
+  },
+
+  create: function(params) {
+    return new Promise(function (resolve, reject) {
+      var EvetnType = new EvetnTypes(params);
+      EvetnType.save(function(err, item) {
+        if (err) {
+          reject( err );
+          return;
+        }
+        resolve( item );
+      });
+    });
+  },
+
+  update: function(id, params) {
+    return new Promise(function (resolve, reject) {
+      var EvetnType = new EvetnTypes(params);
+
+      EvetnTypes.findById(id, function(err, item) {
+        console.log(item);
+        for (var id in params) {
+          item[id] = params[id]
+        }
+        item.save(function(err, item) {
+          if (err) {
+            reject( err );
+            return;
+          }
+          resolve( item );
+        });
+
+      });
+    });
   }
-}
+};
+
